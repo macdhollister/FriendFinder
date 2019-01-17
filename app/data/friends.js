d450efe3026c70c0099950504c841723b3ef0c53
macdh <macdhollister@gmail.com>
2019-01-16 22:14:38 -0800

    added compare user function in post request

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,18 @@
 const users = [
     {
-        "name":"Mac",
-        "photo":"",
-        "scores":[
+        "name": "Mac",
+        "photo": "https://media.licdn.com/dms/image/C5603AQEEkST415_TIQ/profile-displayphoto-shrink_200_200/0?e=1553126400&v=beta&t=yuUJQc9mPtU3g-saTSkeLDP8fr6CgPd5j5jhXrjJ6ag",
+        "scores": [
+            3,
+            4,
+            4,
+            3,
+            4,
             1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
+            3,
+            4,
+            2,
             1
-        ]
-    },
-    {
-        "name":"Mac2",
-        "photo":"",
-        "scores":[
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2
         ]
     }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,19 +4,47 @@ const router = express.Router();
 const path = require('path');
 const data = require('./../data/friends.js');
 
+function compareUsers(arr1, arr2) {
+    let diff = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        diff += Math.abs(arr1[i] - arr2[i]);
+    }
+
+    return diff;
+}
+
+function findBestMatch(user) {
+    const userAnswers = user.scores;
+    let minDiff = 100;
+    let match;
+
+    // for (let other of data) {
+    //     let temp = compareUsers(userAnswers, other.scores);
+    //     if (temp < minDiff) {
+    //         match = other;
+    //     }
+    // }
+
+    data.forEach(function(other) {
+        console.log(other);
+        let temp = compareUsers(userAnswers, other.scores);
+        if (temp < minDiff) match = other;
+    })
+
+    return match;
+}
+
 // Set Routes
 router.get('/api/friends', (req, res) => {
     return res.json(data);
-    res.end();
 })
 
 router.post('/api/friends', (req, res) => {
     // handles incoming survey results
     // handles compatibility logic
-    console.log('test');
     let newUser = req.body;
-    console.log(newUser);
-    res.json(newUser);
+    res.json(findBestMatch(newUser));
+    data.push(newUser);
     res.end();
 })
 
