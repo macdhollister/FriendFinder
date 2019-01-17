0f8db27ef97516ab6f833423bc2b2ec2e3ebb97f
macdh <macdhollister@gmail.com>
2019-01-16 18:36:16 -0800

    Submit button now submits POST request

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,12 +7,17 @@ const data = require('./../data/friends.js');
 // Set Routes
 router.get('/api/friends', (req, res) => {
     return res.json(data);
+    res.end();
 })
 
 router.post('/api/friends', (req, res) => {
     // handles incoming survey results
     // handles compatibility logic
-    
+    console.log('test');
+    let newUser = req.body;
+    console.log(newUser);
+    res.json(newUser);
+    res.end();
 })
 
 module.exports = router;
